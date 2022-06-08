8d7577a54a77ca91ce647c03efabe049cf1b31c9
Olinko36 <akimkinaad@student.bmstu.ru>
2022-06-08 20:51:01 +0300

    commit2

--- src/classes/Vehicle.java
+++ src/classes/Vehicle.java
@@ -1,5 +1,28 @@
 package classes;
 
 public class Vehicle {
+    public String type;
+    public double av_speed;
+    public double consum_100;
 
+    public void time(double S){
+        double time = S/av_speed;
+        String result = String.format("%.2f", time);
+        System.out.println("Время, за которое " + type + " преодолеет дистанцию = " + result + "ч");
+    }
+
+    public void fuel(double S) {
+        double fuel = (S/100)*consum_100;
+        String result = String.format("%.2f", fuel);
+        System.out.println("На " + S + "км потребуется " + result + "л топлива ");
+    }
+
+    public boolean isDigit(String s) throws NumberFormatException {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
--- src/functions/Function2.java
+++ src/functions/Function2.java
@@ -1,16 +1,20 @@
 package functions;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Function2 {
     public static void main(String[] args) {
         int[] a = new int[]{1, 2, 3, 4, 5};
         int[] b = new int[]{-5, -6, -7};
-        // test 1
+// test 1
         System.out.printf("Test 1 %s.\n", ClassForTest.compareResults(merge(a, b), new int[]{-7, -6, -5, 1, 2, 3, 4, 5}) ? "passed" : "failed");
 
-        //test 2
+//test 2
         System.out.printf("Test 2 %s.\n", ClassForTest.compareResults(merge(new int[0], new int[0]), new int[]{}) ? "passed" : "failed");
 
-        //test 3
+//test 3
         System.out.printf("Test 3 %s.\n", ClassForTest.compareResults(merge(b, a), new int[]{5, 4, 3, 2, 1, -5, -6, -7}) ? "passed" : "failed");
 
     }
@@ -18,8 +22,36 @@ public class Function2 {
     /*
     методу передаются два упорядоченных массива(могут быть упорядочены по-разному), реализуйте функцию,
     которая объединит их и вернет упорядоченный массив с порядком таким же как и a
-     */
+    */
     private static int[] merge(int[] a, int[] b) {
-        return new int[]{1};
+        if (a.length == 0 & b.length == 0){
+            return a;
+        } else {
+            int[] helper = new int[a.length + b.length];
+            if (a[0] < a[a.length - 1]) {
+                for (int i = 0; i < a.length; i++) {
+                    helper[i] = a[i];
+                }
+                for (int i = a.length; i < helper.length; i++) {
+                    helper[i] = b[i-a.length];
+                }
+                Arrays.sort(helper);
+            }
+            if (a[0] >= a[a.length - 1]) {
+                for (int i = 0; i < a.length; i++) {
+                    helper[i] = a[i];
+                }
+                for (int i = a.length; i < helper.length; i++) {
+                    helper[i] = b[i-a.length];
+                }
+                Arrays.sort(helper);
+                for (int i = 0; i < helper.length / 2 ; i++){
+                    int h = helper[helper.length - i - 1];
+                    helper [helper.length - i - 1] = helper[i];
+                    helper [i] = h;
+                }
+            }
+            return helper;
+        }
     }
 }
